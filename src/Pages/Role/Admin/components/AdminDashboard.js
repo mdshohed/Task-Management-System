14e8d0ec0d7d0d6612c789c05849f84d96c712e1
mdshohed <mdshohed170@gmail.com>
2023-11-22 01:36:48 +0600

    update

--- src/Pages/Role/Admin/components/AdminDashboard.js
+++ src/Pages/Role/Admin/components/AdminDashboard.js
@@ -1,6 +1,5 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
-import swal from 'sweetalert';
 import useTaskInfo from '../../../../hooks/useTaskInfo';
 
 const AdminDashboard = () => {
